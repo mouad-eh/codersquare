d774cef5f3416b4e18c94476d3b751d33c85e52c
mouad-eh <jm_elhaouari@esi.dz>
2022-06-24 15:05:50 +0100

    cleanup

--- server/server.ts
+++ server/server.ts
@@ -3,18 +3,17 @@ import { createPostHandler, listPostsHandler } from './handlers/postHandlers';
 import asyncHandler from "express-async-handler"; // a problem in express solved by this package
 import { initDb } from './datastore';
 import { signInHandler, signUpHandler } from './handlers/userHandlers';
+import { requestLoggerMiddleware } from './middleware/loggerMiddleWare';
+import { errorHandler } from './middleware/errorMiddleWare';
 
 (async ()=>{
 await initDb();
 const app = express();
 
-const requestLoggerMiddleware: RequestHandler = (req, res, next) => {
-    console.log(req.method, req.path, "- body: ", req.body);
-    next();
-}
 
 app.use(express.json());
-app.use(requestLoggerMiddleware)
+app.use(requestLoggerMiddleware);
+app.use(errorHandler);
 
 app.get("/posts", asyncHandler(listPostsHandler))
 app.post("/posts", asyncHandler(createPostHandler))
@@ -22,12 +21,7 @@ app.post("/posts", asyncHandler(createPostHandler))
 app.post("/signup",asyncHandler(signUpHandler))
 app.post("/signin",asyncHandler(signInHandler))
 
-// middleware handling errors
-const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-    console.error('Uncaught exception: ', err);
-    return res.status(500).send('Oops, an unexpected error occured, please try again!');
-}
-app.use(errorHandler);
+
 
 app.listen(3000);
 })()
